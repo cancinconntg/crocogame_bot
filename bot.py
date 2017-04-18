8ac86bf4f6d7af9e8fd375efc7d0c6e75a186f06
Alexandr Shelemetyev <shelemetyev@katren.ru>
2017-04-18 17:33:35 +0700

    beautify code

--- bot.py
+++ bot.py
@@ -1,37 +1,39 @@
+"""Telegram bot."""
 import requests
-import json
 import time
 import random
 import re
 
-def check_updates():
 
+def check_updates():
+    """Telegram bot cycle."""
     global TOKEN
     global offset
     global words
 
-    URL = 'https://api.telegram.org/bot' # URL на который отправляется запрос
+    URL = 'https://api.telegram.org/bot'
 
     data = {'offset': offset + 1, 'limit': 0, 'timeout': 0}
 
-    try: # обрабатываем исключения
-        request = requests.post(URL + TOKEN + '/getUpdates', data=data) # собственно сам запрос
+    try:
+        request = requests.post(URL + TOKEN + '/getUpdates', data=data)
         assert request.status_code == 200
     except:
         print('Error getting updates', request.request, request.text)
         return False
 
-    if not request.status_code == 200: return False # проверяем пришедший статус ответа
-    if not request.json()['ok']: return False
+    if not request.status_code == 200:
+        return False
+    # проверяем пришедший статус ответа
+    if not request.json()['ok']:
+        return False
 
     for update in request.json()['result']:
-        offset = update['update_id'] #  подтверждаем текущее обновление
+        offset = update['update_id']
 
-        if 'message' not in update or 'text' not in update['message']: # это просто текст или какая-нибудь картиночка?
+        if 'message' not in update or 'text' not in update['message']:
             print('Unknown message')
             continue
-
-
 
         getWord = 'Получить слово'
         count = 1
@@ -41,31 +43,37 @@ def check_updates():
             count = min(int(update['message']['text']), 10)
 
         for i in range(count):
-            text += words[random.randint(0,len(words)-1)]
+            text += words[random.randint(0, len(words) - 1)]
 
-        message_data = { # формируем информацию для отправки сообщения
-            'chat_id': update['message']['chat']['id'], # куда отправляем сообщение
-            'text': text, # само сообщение для отправки
-            'reply_markup': {'keyboard': [[{'text': 'Получить слово'}], [{'text': '5'}, {'text': '10'}]]},
-            # 'reply_to_message_id': update['message']['message_id'], # если параметр указан, то бот отправит сообщение в reply
-            'parse_mode': 'HTML' # про форматирование текста ниже
+        message_data = {
+            'chat_id': update['message']['chat']['id'],
+            'text': text,
+            'reply_markup': {'keyboard': [
+                [{'text': getWord}], [{'text': '5'}, {'text': '10'}]
+            ]},
+            'parse_mode': 'HTML'
         }
 
         try:
-            request = requests.post(URL + TOKEN + '/sendMessage', json=message_data) # запрос на отправку сообщения
+            request = requests.post(
+                URL + TOKEN + '/sendMessage',
+                json=message_data
+                )
             assert request.status_code == 200
         except:
             print('Send message error:', request.request, request.text)
             return False
 
-        if not request.status_code == 200: # проверим статус пришедшего ответа
+        if not request.status_code == 200:
+            # проверим статус пришедшего ответа
             return False
 
+
 with open('.token', 'r') as t:
-    TOKEN=re.sub('^\s+|\n|\r|\s+$','', t.read())
+    TOKEN = re.sub('^\s+|\n|\r|\s+$', '', t.read())
 
 with open('words', 'r') as f:
-	words=f.readlines()
+    words = f.readlines()
 
 offset = 0
 
@@ -73,6 +81,6 @@ while True:
     try:
         check_updates()
         time.sleep(5)
-    except KeyboardInterrupt: # порождается, если бота остановил пользователь
+    except KeyboardInterrupt:
         print('Interrupted by the user')
         break
